736424bdf3a42b19ab0a0902f8a3ac56e84ac3ce
ajaysingh132 <banjarajanmat@gmail.com>
2026-01-20 14:46:32 +0530

    Update service-worker.js
    Signed-off-by: ajaysingh132 <banjarajanmat@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,34 @@
-const CACHE_NAME = "gormati-hak-cache-v1";
-
-const urlsToCache = [
+const CACHE_NAME = "ghp-cache-v1";
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js"
+  "./notice.html",
+  "./admin.html",
+  "./app.js",
+  "./notice.js",
+  "./admin.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
